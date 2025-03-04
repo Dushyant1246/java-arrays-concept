54d41636f31705d4a6d262cc3f472e7b4e5d3e85
Dushyant1246 <dushyant.dhmn808@gmail.com>
2025-03-04 14:02:58 +0530

    [Dushyant] ADD: Find largest and second largest digit

--- LargestDigit.java
+++ LargestDigit.java
@@ -6,37 +6,36 @@ class LargestDigit {
         // Create a Scanner object for user input
         Scanner scanner = new Scanner(System.in);
         
-		// Variable to store maximum digit size
-		int maxDigit = 10;
+	// Variable to store maximum digit size
+	int maxDigit = 10;
 		
         // Prompt user to enter a number
         System.out.print("Enter a number ( " + maxDigit + " maximum digits): ");
         int number = scanner.nextInt();
         
         // Loop to add digits to an array
-		int[] digits = new int[maxDigit];
-		int count = 0;
+	int[] digits = new int[maxDigit];
+	int count = 0;
         while (number != 0) {
-			if (count >= maxDigit) {
-				System.err.println("Maximum limit exceeded");
-				System.exit(-1);
-			}
-			digits[count] = number % 10;
-            number /= 10;
-            count++;
+		if (count >= maxDigit) {
+			break;
+		}
+		digits[count] = number % 10;
+		number /= 10;
+		count++;
         }
         
-		// Find the largest and second largest digits
-		int largest = 0;
-		int secondLargest = 0;
-		for (int i = 0 ; i < digits.length ; i++) {
-			if (digits[i] > largest) {
-				largest = digits[i];
-			}
-			if (digits[i] > secondLargest && digits[i] != largest) {
-				secondLargest = digits[i];
-			}
+	// Find the largest and second largest digits
+	int largest = 0;
+	int secondLargest = 0;
+	for (int i = 0 ; i < digits.length ; i++) {
+		if (digits[i] > largest) {
+			largest = digits[i];
 		}
+		if (digits[i] > secondLargest && digits[i] != largest) {
+			secondLargest = digits[i];
+		}
+	}
 		
         // Display the count
         System.out.println("Largest digit: " + largest);
